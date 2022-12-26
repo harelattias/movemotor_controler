4ab07f7d48853ff00a4104dec2bbb15c584b1911
ד"ר מיאו <109675030+harelattias@users.noreply.github.com>
2022-12-26 11:03:24 +0200

    motor speed calculation is on controller v1.0

--- main.ts
+++ main.ts
@@ -9,6 +9,7 @@ input.onButtonPressed(Button.B, function () {
 radio.onReceivedValue(function (name, value) {
     serial.writeValue(name, value)
 })
+let speed_left_motor = 0
 let speed_right = 0
 let speed_fwd = 0
 radio.setGroup(111)
@@ -16,10 +17,18 @@ basic.forever(function () {
 	
 })
 loops.everyInterval(250, function () {
-    speed_fwd = Math.round(input.acceleration(Dimension.Y) / -10.23)
-    radio.sendValue("cmd_fwd", speed_fwd)
-    serial.writeValue("forward", speed_fwd)
-    speed_right = Math.round(input.acceleration(Dimension.X) / 10.23)
-    radio.sendValue("cmd_rgt", speed_right)
-    serial.writeValue("right", speed_right)
+    speed_fwd = Math.round(input.acceleration(Dimension.Y) / -15.1)
+    if (speed_fwd > -21.1 && speed_fwd < 21.1) {
+        speed_fwd = 0
+    }
+    speed_right = Math.round(input.acceleration(Dimension.X) / 30.69)
+    if (speed_right > -7 && speed_right < 7) {
+        speed_right = 0
+    }
+    speed_right = speed_fwd + speed_right
+    speed_left_motor = speed_fwd - speed_right
+    radio.sendValue("rgt_spd", speed_fwd)
+    serial.writeValue("rgt_spd", speed_fwd)
+    radio.sendValue("left_spd", speed_right)
+    serial.writeValue("left_spd", speed_right)
 })
