530d4ce4da87535fa2914c68149e55e722de5b6f
ד"ר מיאו <109675030+harelattias@users.noreply.github.com>
2023-01-25 16:27:54 +0200

    v1.3 - gear works

--- main.py
+++ main.py
@@ -1,31 +1,129 @@
 def on_button_pressed_a():
-    radio.send_string("beep")
-    basic.show_icon(IconNames.HAPPY)
+    global is_joystik
+    is_joystik = 0
+    basic.show_leds("""
+        . . . . .
+                . . . . .
+                . # # # .
+                . # # # .
+                . # # # .
+    """)
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
 def on_button_pressed_b():
-    radio.send_string("beeeeeeeep")
-    basic.show_icon(IconNames.SAD)
+    global is_joystik
+    is_joystik = 1
+    basic.show_leds("""
+        . . . . .
+                . . . . .
+                . # # # .
+                # # # # #
+                # # . # #
+    """)
 input.on_button_pressed(Button.B, on_button_pressed_b)
+
+def show_gear():
+    if gear == 1:
+        led.plot(0, 0)
+        led.unplot(1, 0)
+        led.unplot(2, 0)
+    else:
+        if gear == 2:
+            led.plot(0, 0)
+            led.plot(1, 0)
+            led.unplot(2, 0)
+        else:
+            if gear == 3:
+                led.plot(0, 0)
+                led.plot(1, 0)
+                led.plot(2, 0)
 
 def on_received_value(name, value):
     serial.write_value(name, value)
 radio.on_received_value(on_received_value)
 
+def on_logo_pressed():
+    radio.send_string("beep")
+input.on_logo_event(TouchButtonEvent.PRESSED, on_logo_pressed)
+
+speed_left_motor = 0
+speed_right_motor = 0
 speed_right = 0
 speed_fwd = 0
+gear = 0
+is_joystik = 0
+is_joystik = 0
+basic.show_leds("""
+    . . . . .
+        . . . . .
+        . # # # .
+        . # # # .
+        . # # # .
+""")
 radio.set_group(111)
+gear = 2
+speed_multiplier = 1
 
 def on_forever():
-    pass
+    if GHBit.button(GHBit.enButton.B4, GHBit.enButtonState.REALSE):
+        radio.send_string("off_off")
+    if GHBit.rocker(GHBit.enRocker.PRESS):
+        radio.send_string("beep")
+    if GHBit.button(GHBit.enButton.B4, GHBit.enButtonState.PRESS):
+        radio.send_string("off_lig")
 basic.forever(on_forever)
 
 def on_every_interval():
-    global speed_fwd, speed_right
-    speed_fwd = Math.round(input.acceleration(Dimension.Y) / -10.23)
-    radio.send_value("speed_forward", speed_fwd)
-    serial.write_value("acc_y", speed_fwd)
-    speed_right = Math.round(input.acceleration(Dimension.X) / 10.23)
-    radio.send_value("speed_right", speed_right)
-    serial.write_value("acc_x", speed_right)
+    global speed_fwd, speed_right, speed_right_motor, speed_left_motor
+    if is_joystik == 0:
+        speed_fwd = Math.round(input.acceleration(Dimension.Y) / -15.1)
+        if speed_fwd > -21.1 and speed_fwd < 21.1:
+            speed_fwd = 0
+        speed_right = Math.round(input.acceleration(Dimension.X) / 30.69)
+        if speed_right > -7 and speed_right < 7:
+            speed_right = 0
+    elif is_joystik == 1:
+        if GHBit.rocker(GHBit.enRocker.UP):
+            speed_fwd = 66
+            speed_right = 0
+        if GHBit.rocker(GHBit.enRocker.NOSTATE):
+            speed_fwd = 0
+            speed_right = 0
+        if GHBit.rocker(GHBit.enRocker.DOWN):
+            speed_fwd = -66
+            speed_right = 0
+        if GHBit.rocker(GHBit.enRocker.RIGHT):
+            speed_fwd = 0
+            speed_right = 25
+        if GHBit.rocker(GHBit.enRocker.LEFT):
+            speed_fwd = 0
+            speed_right = -25
+    speed_right_motor = (speed_fwd - speed_right) * speed_multiplier
+    speed_left_motor = (speed_fwd + speed_right) * speed_multiplier
+    radio.send_value("rgt_spd", speed_right_motor)
+    serial.write_value("rgt_spd", speed_right_motor)
+    radio.send_value("left_spd", speed_left_motor)
+    serial.write_value("left_spd", speed_left_motor)
 loops.every_interval(250, on_every_interval)
+
+def on_every_interval2():
+    global gear, speed_multiplier
+    if GHBit.button(GHBit.enButton.B2, GHBit.enButtonState.PRESS):
+        gear += 1
+        if gear > 3:
+            gear = 3
+        show_gear()
+        serial.write_value("gear", gear)
+    if GHBit.button(GHBit.enButton.B3, GHBit.enButtonState.PRESS):
+        gear += -1
+        if gear < 1:
+            gear = 1
+        show_gear()
+        serial.write_value("gear", gear)
+    if gear == 1:
+        speed_multiplier = 0.75
+    if gear == 2:
+        speed_multiplier = 1
+    if gear == 3:
+        speed_multiplier = 1.5
+loops.every_interval(200, on_every_interval2)
